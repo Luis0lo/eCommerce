45bcceefcbfe8e17f190a0aec963bcbfd45ffec6
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-07-08 11:13:11 +0100

    Product page

---
+++ src/pages/Product.js
@@ -0,0 +1,144 @@
+import styled from 'styled-components';
+import { Remove, Add } from '@material-ui/icons';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import NewsLetter from '../components/Newsletter';
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 75vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+const Title = styled.h1`
+  font-weight: 200;
+`;
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 30px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://i.etsystatic.com/25902233/r/il/6fdc49/3688393606/il_794xN.3688393606_1kw2.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Sunflower</Title>
+          <Description>
+            Crochet sunflowers, sunflower, faux flowers, Mother’s Day, birthday
+            gifts, flowers, summer flowers, spring, Easter, home decor.
+          </Description>
+          <Price>£ 7.5</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="yellow" />
+              <FilterColor color="black" />
+              <FilterColor color="blue" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>SMALL</FilterSizeOption>
+                <FilterSizeOption>MEDIUM</FilterSizeOption>
+                <FilterSizeOption>LARGE</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <NewsLetter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
