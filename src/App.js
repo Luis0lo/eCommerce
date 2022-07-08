0ddf4defebad7e90b4c5bc3ec79ed1202346ef03
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-07-08 15:00:30 +0100

    Register, Login components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 // import Home from './pages/Home';
-import ProductList from './pages/ProductList';
+// import ProductList from './pages/ProductList';
+// import Login from './pages/Login';
+import Register from './pages/Register';
 // import Product from './pages/Product';
 
 const App = () => {
-  // return <Product />;
-  return <ProductList />;
+  return <Register />;
 };
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,79 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url('https://images.unsplash.com/photo-1595341595379-cf1cb694ea1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80')
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 45%;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 0px 0px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  border: none;
+  padding: 10px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  border-radius: 5px;
+  margin: 10px 0;
+`;
+const Link = styled.a`
+  margin: 5px 0;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username"></Input>
+
+          <Input placeholder="password"></Input>
+
+          <Button>LOGIN</Button>
+          <Link>Do not remember the password?</Link>
+          <Link>Create a new account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
