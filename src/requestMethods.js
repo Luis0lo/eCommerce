b349396551da92ac3f76ee1ab5cc8cd198b0963b
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-07-16 17:08:47 +0100

    get token from localstorage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyY2QxOWNmNTE2NTRhZTRhYmVkNzE4YiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzczMDE0NiwiZXhwIjoxNjU3OTg5MzQ2fQ.3peXdFhsYj8w7MjJWJMvW-nET797F84qElyVNMTVj_g';
-
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+  .currentUser?.accessToken;
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
